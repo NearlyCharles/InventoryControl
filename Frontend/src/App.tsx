c37d94f64970b47e85227cb959d33f1ef74896b0
NearlyCharles <charlesjameshudson@gmail.com>
2024-07-10 22:32:45 -0500

    homeview html, deleted package

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -13,3 +13,18 @@
 // }
 
 // export default App
+
+import './App.scss'
+import React from "react"
+import HomeView from './views/Home/HomeView'
+ import { Outlet } from 'react-router-dom'
+
+ function App() {
+
+   return (
+     <div className='app-container'> 
+     </div>
+   )
+ }
+
+ export default App
