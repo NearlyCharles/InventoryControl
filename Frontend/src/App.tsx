c6f18005450ea6e2d6e966751c3cbef3755c07e4
NearlyCharles <charlesjameshudson@gmail.com>
2024-07-10 23:19:55 -0500

    graphstuff

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -14,17 +14,3 @@
 
 // export default App
 
-import './App.scss'
-import React from "react"
-import HomeView from './views/Home/HomeView'
- import { Outlet } from 'react-router-dom'
-
- function App() {
-
-   return (
-     <div className='app-container'> 
-     </div>
-   )
- }
-
- export default App
--- Frontend/src/views/Home/HomeView.tsx
+++ Frontend/src/views/Home/HomeView.tsx
@@ -1,15 +1,85 @@
 
-
+import "./HomeView.scss"
 import React from 'react';
-import BarChart from '../Componets/BarChart'
-
 function HomeView() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Chart.js Bar Chart in React</h1>
-        <BarChart />
-      </header>
+    <div className="dashboard-container">
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+      <div className="graph-container">
+        GRAPH
+      </div>
+     
     </div>
   );
 }
