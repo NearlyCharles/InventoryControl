dbd0c18195f00e8bb1a7c3b48503ce84678c9c71
NearlyCharles <charlesjameshudson@gmail.com>
2024-07-13 09:18:17 -0500

    started checkout

--- Frontend/src/views/Checkout/CheckoutView.tsx
+++ Frontend/src/views/Checkout/CheckoutView.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./Checkout.scss";
 
 function CheckoutView() {
   return (
-    <div>CheckoutView</div>
-  )
+    <div>
+      <div>CheckoutView</div>
+      <div className="info-box">
+        <input type="text"></input>
+      </div>
+    </div>
+  );
 }
 
-export default CheckoutView
+export default CheckoutView;
