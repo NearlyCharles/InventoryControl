a302abbad5cea7eacb1e06b28116f1232e021057
NearlyCharles <charlesjameshudson@gmail.com>
2024-07-20 16:56:42 -0500

    bar chart text change

--- Frontend/src/components/SalesForecast/SalesForecast.tsx
+++ Frontend/src/components/SalesForecast/SalesForecast.tsx
@@ -1,107 +1,109 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Radio from '@mui/material/Radio';
-import { BarChart } from '@mui/x-charts/BarChart';
-import { axisClasses } from '@mui/x-charts/ChartsAxis';
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { axisClasses } from "@mui/x-charts/ChartsAxis";
 
 type TickParamsSelectorProps = {
-  tickPlacement: 'end' | 'start' | 'middle' | 'extremities';
-  tickLabelPlacement: 'tick' | 'middle';
+  tickPlacement: "end" | "start" | "middle" | "extremities";
+  tickLabelPlacement: "tick" | "middle";
   setTickPlacement: React.Dispatch<
-    React.SetStateAction<'end' | 'start' | 'middle' | 'extremities'>
+    React.SetStateAction<"end" | "start" | "middle" | "extremities">
   >;
-  setTickLabelPlacement: React.Dispatch<React.SetStateAction<'tick' | 'middle'>>;
+  setTickLabelPlacement: React.Dispatch<
+    React.SetStateAction<"tick" | "middle">
+  >;
 };
 export const dataset = [
-    {
-      london: 59,
-      paris: 57,
-      newYork: 86,
-      seoul: 21,
-      month: 'Jan',
-    },
-    {
-      london: 50,
-      paris: 52,
-      newYork: 78,
-      seoul: 28,
-      month: 'Feb',
-    },
-    {
-      london: 47,
-      paris: 53,
-      newYork: 106,
-      seoul: 41,
-      month: 'Mar',
-    },
-    {
-      london: 54,
-      paris: 56,
-      newYork: 92,
-      seoul: 73,
-      month: 'Apr',
-    },
-    {
-      london: 57,
-      paris: 69,
-      newYork: 92,
-      seoul: 99,
-      month: 'May',
-    },
-    {
-      london: 60,
-      paris: 63,
-      newYork: 103,
-      seoul: 144,
-      month: 'June',
-    },
-    {
-      london: 59,
-      paris: 60,
-      newYork: 105,
-      seoul: 319,
-      month: 'July',
-    },
-    {
-      london: 65,
-      paris: 60,
-      newYork: 106,
-      seoul: 249,
-      month: 'Aug',
-    },
-    {
-      london: 51,
-      paris: 51,
-      newYork: 95,
-      seoul: 131,
-      month: 'Sept',
-    },
-    {
-      london: 60,
-      paris: 65,
-      newYork: 97,
-      seoul: 55,
-      month: 'Oct',
-    },
-    {
-      london: 67,
-      paris: 64,
-      newYork: 76,
-      seoul: 48,
-      month: 'Nov',
-    },
-    {
-      london: 61,
-      paris: 70,
-      newYork: 103,
-      seoul: 25,
-      month: 'Dec',
-    },
-  ];
+  {
+    london: 59,
+    paris: 57,
+    newYork: 86,
+    seoul: 21,
+    month: "Jan",
+  },
+  {
+    london: 50,
+    paris: 52,
+    newYork: 78,
+    seoul: 28,
+    month: "Feb",
+  },
+  {
+    london: 47,
+    paris: 53,
+    newYork: 106,
+    seoul: 41,
+    month: "Mar",
+  },
+  {
+    london: 54,
+    paris: 56,
+    newYork: 92,
+    seoul: 73,
+    month: "Apr",
+  },
+  {
+    london: 57,
+    paris: 69,
+    newYork: 92,
+    seoul: 99,
+    month: "May",
+  },
+  {
+    london: 60,
+    paris: 63,
+    newYork: 103,
+    seoul: 144,
+    month: "June",
+  },
+  {
+    london: 59,
+    paris: 60,
+    newYork: 105,
+    seoul: 319,
+    month: "July",
+  },
+  {
+    london: 65,
+    paris: 60,
+    newYork: 106,
+    seoul: 249,
+    month: "Aug",
+  },
+  {
+    london: 51,
+    paris: 51,
+    newYork: 95,
+    seoul: 131,
+    month: "Sept",
+  },
+  {
+    london: 60,
+    paris: 65,
+    newYork: 97,
+    seoul: 55,
+    month: "Oct",
+  },
+  {
+    london: 67,
+    paris: 64,
+    newYork: 76,
+    seoul: 48,
+    month: "Nov",
+  },
+  {
+    london: 61,
+    paris: 70,
+    newYork: 103,
+    seoul: 25,
+    month: "Dec",
+  },
+];
 
 function SalesForecast({
   tickPlacement,
@@ -110,7 +112,11 @@ function SalesForecast({
   setTickLabelPlacement,
 }: TickParamsSelectorProps) {
   return (
-    <Stack direction="column" justifyContent="space-between" sx={{ width: '100%' }}>
+    <Stack
+      direction="column"
+      justifyContent="space-between"
+      sx={{ width: "100%" }}
+    >
       <FormControl>
         <FormLabel id="tick-placement-radio-buttons-group-label">
           tickPlacement
@@ -122,7 +128,7 @@ function SalesForecast({
           value={tickPlacement}
           onChange={(event) =>
             setTickPlacement(
-              event.target.value as 'start' | 'end' | 'middle' | 'extremities',
+              event.target.value as "start" | "end" | "middle" | "extremities"
             )
           }
         >
@@ -146,7 +152,7 @@ function SalesForecast({
           name="label-placement"
           value={tickLabelPlacement}
           onChange={(event) =>
-            setTickLabelPlacement(event.target.value as 'tick' | 'middle')
+            setTickLabelPlacement(event.target.value as "tick" | "middle")
           }
         >
           <FormControlLabel value="tick" control={<Radio />} label="tick" />
@@ -162,32 +168,37 @@ const valueFormatter = (value: number | null) => `${value}mm`;
 const chartSetting = {
   yAxis: [
     {
-      label: 'rainfall (mm)',
+      label: "Gross sales",
     },
   ],
-  series: [{ dataKey: 'seoul', label: 'Seoul rainfall', valueFormatter }],
+  series: [{ dataKey: "seoul", label: "Sales Report", valueFormatter }],
   height: 300,
   sx: {
     [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
-      transform: 'translateX(-10px)',
+      transform: "translateX(-10px)",
     },
   },
 };
 
 export default function TickPlacementBars() {
   const [tickPlacement, setTickPlacement] = React.useState<
-    'start' | 'end' | 'middle' | 'extremities'
-  >('middle');
+    "start" | "end" | "middle" | "extremities"
+  >("middle");
   const [tickLabelPlacement, setTickLabelPlacement] = React.useState<
-    'middle' | 'tick'
-  >('middle');
+    "middle" | "tick"
+  >("middle");
 
   return (
-    <div style={{ width: '100%' }}>
+    <div style={{ width: "100%" }}>
       <BarChart
         dataset={dataset}
         xAxis={[
-          { scaleType: 'band', dataKey: 'month', tickPlacement, tickLabelPlacement },
+          {
+            scaleType: "band",
+            dataKey: "month",
+            tickPlacement,
+            tickLabelPlacement,
+          },
         ]}
         {...chartSetting}
       />
